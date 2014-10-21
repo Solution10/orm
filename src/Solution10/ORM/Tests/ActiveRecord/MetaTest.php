1a5ce93a661a64c4d073c5997fb9988261b1d11a
Alex Gisby <alex@solution10.com>
2014-10-21 21:52:31 +0100

    Improving meta tests.

--- src/Solution10/ORM/Tests/ActiveRecord/MetaTest.php
+++ src/Solution10/ORM/Tests/ActiveRecord/MetaTest.php
@@ -3,6 +3,7 @@
 namespace Solution10\ORM\Tests\ActiveRecord;
 
 use Solution10\ORM\ActiveRecord\Meta;
+use Solution10\ORM\ActiveRecord\Field\String;
 
 class MetaTest extends \PHPUnit_Framework_TestCase
 {
@@ -22,5 +23,39 @@ class MetaTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('default', $meta->connection());
         $this->assertEquals($meta, $meta->connection('unittests'));
         $this->assertEquals('unittests', $meta->connection());
+    }
+
+    public function testGetSetStringPk()
+    {
+        $meta = new Meta('MyApp\\Models\\User');
+        $this->assertEquals('id', $meta->primaryKey());
+        $this->assertEquals($meta, $meta->primaryKey('user_id'));
+        $this->assertEquals('user_id', $meta->primaryKey());
+    }
+
+    public function testGetSetArrayPk()
+    {
+        $meta = new Meta('MyApp\\Models\\User');
+
+        $pk = ['id', 'email']; // Don't do this for real...
+
+        $this->assertEquals('id', $meta->primaryKey());
+        $this->assertEquals($meta, $meta->primaryKey($pk));
+        $this->assertEquals($pk, $meta->primaryKey());
+    }
+
+    public function testSetGetField()
+    {
+        $field = new String();
+        $meta = new Meta('MyApp\\Models\\User');
+
+        $this->assertEquals($meta, $meta->field('username', $field));
+        $this->assertEquals($field, $meta->field('username'));
+    }
+
+    public function testGetUnknownField()
+    {
+        $meta = new Meta('MyApp\\Models\\User');
+        $this->assertNull($meta->field('unknown'));
     }
 }

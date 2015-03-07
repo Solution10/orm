6ca688be9bf9183b4a65bdf0a61e320493a9c192
Alex Gisby <alex@solution10.com>
2015-03-07 16:16:31 +0000

    Fixing repo unit tests.

--- tests/Repository/Stubs/UsersRepo.php
+++ tests/Repository/Stubs/UsersRepo.php
@@ -3,8 +3,19 @@
 namespace Solution10\ORM\Tests\Repository\Stubs;
 
 use Solution10\ORM\Repository\Repo;
+use Solution10\ORM\Repository\RepoItemInterface;
 
 class UsersRepo extends Repo
 {
-
+    /**
+     * Persists an item into the repo. That item might already exist, or
+     * it might be a totally new one to create.
+     *
+     * @param       RepoItemInterface $item Item to store
+     * @return      $this
+     */
+    public function store(RepoItemInterface $item)
+    {
+        // TODO: Implement store() method.
+    }
 }
